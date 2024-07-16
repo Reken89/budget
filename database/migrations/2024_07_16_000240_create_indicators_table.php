d4737769264e07ea1ce1ac9552679c6b3a9ad9f1
Administrator <Administrator@ADMIN.domenfo.com>
2024-07-16 16:32:20 +0300

    Разработка модуля 1С

---
+++ database/migrations/2024_07_16_000240_create_indicators_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIndicatorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('indicators', function (Blueprint $table) {
+            $table->id();
+            $table->string('shared', 10);
+            $table->string('main', 10);
+            $table->integer('number');
+            $table->integer('code');
+            $table->string('title', 1000);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('indicators');
+    }
+}
+
