7702fa0301bf6c485eb37f45aa088df303285268
Administrator <Administrator@ADMIN.domenfo.com>
2025-09-11 14:23:59 +0300

    Правка seeder для меньшего потребления памяти

--- database/seeders/FillingArchiveSeeder.php
+++ database/seeders/FillingArchiveSeeder.php
@@ -15,38 +15,41 @@ class FillingArchiveSeeder extends Seeder
      */
     public function run()
     {
-        for($m = 1; $m < 13; $m++){
-            //Получаем информацию ОФС
-            $info = Ofs::select()
-                ->where('mounth', $m) 
-                ->get()
-                ->toArray();
+        for($chapter = 1; $chapter < 6; $chapter++){
+            for($m = 1; $m < 13; $m++){
+                //Получаем информацию ОФС
+                $info = Ofs::select()
+                    ->where('mounth', $m) 
+                    ->where('chapter', $chapter) 
+                    ->get()
+                    ->toArray();
 
-            //Добавляем значения в таблицу communal
-            foreach ($info as $value) {
-                Archive::create([
-                    'user_id'          => $value['user_id'],
-                    'ekr_id'           => $value['ekr_id'],
-                    'year'             => $value['year'],
-                    'mounth'           => $value['mounth'],
-                    'chapter'          => $value['chapter'],
-                    'status'           => 1,
-                    'lbo'              => $value['lbo'],
-                    'prepaid'          => $value['prepaid'],
-                    'credit_year_all'  => $value['credit_year_all'],
-                    'credit_year_term' => $value['credit_year_term'],
-                    'debit_year_all'   => $value['debit_year_all'],
-                    'debit_year_term'  => $value['debit_year_term'],
-                    'fact_all'         => $value['fact_all'],
-                    'fact_mounth'      => $value['fact_mounth'],
-                    'kassa_all'        => $value['kassa_all'],
-                    'kassa_mounth'     => $value['kassa_mounth'],
-                    'credit_end_all'   => $value['credit_end_all'],
-                    'credit_end_term'  => $value['credit_end_term'],
-                    'debit_end_all'    => $value['debit_end_all'],
-                    'debit_end_term'   => $value['debit_end_term'],
-                    'return_old_year'  => $value['return_old_year'],
-                ]);                                                                      
+                //Добавляем значения в таблицу communal
+                foreach ($info as $value) {
+                    Archive::create([
+                        'user_id'          => $value['user_id'],
+                        'ekr_id'           => $value['ekr_id'],
+                        'year'             => $value['year'],
+                        'mounth'           => $value['mounth'],
+                        'chapter'          => $value['chapter'],
+                        'status'           => 1,
+                        'lbo'              => $value['lbo'],
+                        'prepaid'          => $value['prepaid'],
+                        'credit_year_all'  => $value['credit_year_all'],
+                        'credit_year_term' => $value['credit_year_term'],
+                        'debit_year_all'   => $value['debit_year_all'],
+                        'debit_year_term'  => $value['debit_year_term'],
+                        'fact_all'         => $value['fact_all'],
+                        'fact_mounth'      => $value['fact_mounth'],
+                        'kassa_all'        => $value['kassa_all'],
+                        'kassa_mounth'     => $value['kassa_mounth'],
+                        'credit_end_all'   => $value['credit_end_all'],
+                        'credit_end_term'  => $value['credit_end_term'],
+                        'debit_end_all'    => $value['debit_end_all'],
+                        'debit_end_term'   => $value['debit_end_term'],
+                        'return_old_year'  => $value['return_old_year'],
+                    ]);                                                                      
+                }
             }
         }
     }       
